cdd1fd2d9a52e4377b35e3851c3f6a8e00276b69
Chris Closser <chrisclosser1@gmail.com>
2017-02-10 21:22:30 -0800

    made some symantic changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 'use strict';
 // THE FIRST LINE SHOULD ALWAYS BE...'use strict'
 var correct = 0;
-var takeQuiz = confirm('Hello, would you like to take a short quiz about me, Chris?');
+var takeQuiz = confirm ('Hello, would you like to take a short quiz about me, Chris?');
 
-if(takeQuiz) {
-  console.log('Awesome! Lets get started!');
-  document.write('<h1>Awesome! Lets get started!</h1>');
-  alert('Awesome! Lets get started!');
+if (takeQuiz) {
+  console.log ('Awesome! Lets get started!');
+  document.write ('<h1>Awesome! Lets get started!</h1>');
+  alert ('Awesome! Lets get started!');
 } else {
     // console.log('Too bad starting anyway!');
     // document.write('<h1>Too bad starting anyway!</h2>');
@@ -21,44 +21,44 @@ while (takeQuiz) {
   // first question if user clicks ok
   var yesNo = 'Please answer with a Y or N. \n';
   var askA = 'Do I have a dog?';
-  var answer = prompt(yesNo + askA).toUpperCase();
+  var answer = prompt (yesNo + askA) .toUpperCase ();
   // while (answer != 'Y' && answer != 'N') {
   //   var answer = prompt(answer + ' Is not a valid input. Please type either a Y or N').toUpperCase();
   // }
 
-  function questionA (){
+  function questionA () {
     // inputVal();
-    if(answer === 'Y') {
-      console.log('Sorry I do not have a dog.');
-      document.write(askA + '<h2>Sorry I do not have a dog.</h2>');
-      alert('Sorry I do not have a dog.');
+    if (answer === 'Y') {
+      console.log ('Sorry, I do not have a dog.');
+      document.write (askA + '<h2>Sorry I do not have a dog.</h2>');
+      alert('Sorry, I do not have a dog.');
     } else {
       correct += 1;
-      console.log('You are correct! I do not have a dog.');
-      document.write(askA + '<h2>You are correct!</h2>');
-      alert('You are correct! I do not have a dog.');
+      console.log ('You are correct! I do not have a dog.');
+      document.write (askA + '<h2>You are correct!</h2>');
+      alert ('You are correct! I do not have a dog.');
     }
-  };
+  }
 
   // Q #2
   function questionB () {
     var askB = 'Am I older than 40?';
-    answer = prompt(yesNo + askB);
+    answer = prompt (yesNo + askB);
 
-    if(answer === 'Y') {
+    if (answer === 'Y') {
       console.log('Shame on you! Not quite.');
       document.write(askB + '<h3>Shame on you! Not quite.</h3>');
-      alert('Shame on you! Not quite.');
+      alert ('Shame on you! Not quite.');
     } else {
       correct += 1;
-      console.log('That\'s right, not there yet!');
-      document.write(askB + '<h3>That\'s right, not there yet!</h3>');
-      alert('That\'s right, not there yet!');
+      console.log ('That\'s right, not there yet!');
+      document.write (askB + '<h3>That\'s right, not there yet!</h3>');
+      alert ('That\'s right, not there yet!');
     }
   };
 
   // Q #3
-  function questionC() {
+  function questionC () {
     var askC = 'Do I like pizza?';
     answer = prompt(yesNo + askC).toUpperCase();
 
@@ -75,7 +75,7 @@ while (takeQuiz) {
   };
 
   // Q #4
-  function questionD() {
+  function questionD () {
     var askD = 'Do I like to gamble?';
     var answer = prompt(yesNo + askD).toUpperCase();
 
@@ -86,7 +86,7 @@ while (takeQuiz) {
     } else {
       correct += 1;
       console.log('Right! It\'s bad enough all of that money leaves my possession and I get nothing.');
-      document.write(askD + '<h5>Right! It\'s bad enough all of that money leaves my possession and I get nothing.</h5>');
+      document.write( askD + '<h5>Right! It\'s bad enough all of that money leaves my possession and I get nothing.</h5>');
       alert('Right! It\'s bad enough all of that money leaves my possession and I get nothing.');
     }
   };
@@ -97,13 +97,13 @@ while (takeQuiz) {
     var answer = prompt(yesNo + askE).toUpperCase();
 
     if(answer === 'Y') {
-      console.log('I wish! 12 people have been on the moon and no one since 1972.');
-      document.write(askE + '<h6>I wish! 12 people have been on the moon and no one since 1972.</h6>');
+      console.log ('I wish! 12 people have been on the moon and no one since 1972.');
+      document.write (askE + '<h6>I wish! 12 people have been on the moon and no one since 1972.</h6>');
       alert('I wish! 12 people have been on the moon and no one since 1972.');
     } else {
       correct += 1;
-      console.log('Yup you are right only 12 people  have been on the moon and I am not one.');
-      document.write(askE + '<h6>Yup you are right only 12 people  have been on the moon and I am not one.</h6>');
+      console.log ('Yup you are right only 12 people  have been on the moon and I am not one.');
+      document.write (askE + '<h6>Yup you are right only 12 people  have been on the moon and I am not one.</h6>');
       alert('Yup you are right only 12 people  have been on the moon and I am not one.');
     }
   };
@@ -112,25 +112,25 @@ while (takeQuiz) {
   function questionF () {
     var userNum = prompt('I am thinking of a number between 1 and 20.' /*'\nYou have ' /*+ tries*/);
   // need to figure out how to exit loop if answer is correct
-    var myNum = Math.floor(Math.random() * 20 + 1);
+    var myNum = Math.floor (Math.random () * 20 + 1);
     for (var i = 0; i < 4; i++) {
-      userNum = parseInt(userNum);
+      userNum = parseInt (userNum);
       //console.log(typeof userNum);
-      console.log(myNum);
-      if(userNum !== myNum) {
-        if(i !== 3) {
-          if(userNum > myNum) {
-            console.log('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a smaller number.'/*\nYou have ' + tries + 'more tries.'*/);
-            userNum = prompt('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a smaller number.' /*'\nYou have '+ tries + 'more tries.'*/);
+      console.log (myNum);
+      if (userNum !== myNum) {
+        if (i !== 3) {
+          if (userNum > myNum) {
+            console.log ('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a smaller number.'/*\nYou have ' + tries + 'more tries.'*/);
+            userNum = prompt ('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a smaller number.' /*'\nYou have '+ tries + 'more tries.'*/);
           } else {
-            console.log('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a larger number.'/*\nYou have ' + tries + 'more tries.'*/);
-            userNum = prompt('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a larger number.' /*'\nYou have '+ tries + 'more tries.'*/);
+            console.log ('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a larger number.'/*\nYou have ' + tries + 'more tries.'*/);
+            userNum = prompt ('I\'m sorry ' + userNum + ' is not the number I was thinking of. Try a larger number.' /*'\nYou have '+ tries + 'more tries.'*/);
           }
         }
       } else {
         correct += 1;
-        console.log('You are correct! I was thinking about ' + userNum + '.');
-        alert('You are correct! I was thinking about ' + userNum + '.');
+        console.log ('You are correct! I was thinking about ' + userNum + '.');
+        alert ('You are correct! I was thinking about ' + userNum + '.');
         break;
       }
     }
@@ -138,35 +138,35 @@ while (takeQuiz) {
   // Q #7 where have I lived
   function questionG () {
     var myHomes = ['California', 'Texas', 'Oklahoma'];
-    var userAns = prompt('Besides Washington, what other states do you think I lived?');
+    var userAns = prompt ('Besides Washington, what other states do you think I lived?');
     // need to use wile less than 6
     for (var i = 0; i < 6; i++) {
-      while(i <= myHomes.lenght) {
-        if(userAns !== myHomes[i]){
-          console.log('I\'m sorry please try again. \nI have never lived in ' + userAns);
-          alert('I\'m sorry please try again. \nI have never lived in ' + userAns);
+      while (i <= myHomes.lenght) {
+        if (userAns !== myHomes[i]) {
+          console.log ('I\'m sorry please try again. \nI have never lived in ' + userAns);
+          alert ('I\'m sorry please try again. \nI have never lived in ' + userAns);
           // ii++;
         } else {
           correct += 1;
-          console.log('You are correct, I have lived in ' + userAns);
-          alert('You are correct, I have lived in ' + userAns);
+          console.log ('You are correct, I have lived in ' + userAns);
+          alert ('You are correct, I have lived in ' + userAns);
           break;
         }
       }
     }
   }
-  questionA();
-  questionB();
-  questionC();
-  questionD();
-  questionE();
-  questionF();
-  questionG();
+  questionA ();
+  questionB ();
+  questionC ();
+  questionD ();
+  questionE ();
+  questionF ();
+  questionG ();
 
-  console.log(myHomes);
-  console.log('You got ' + correct + ' of 7 answers correct!');
+  console.log (myHomes);
+  console.log ('You got ' + correct + ' of 7 answers correct!');
   // document.write();
-  takeQuiz = confirm(myHomes + '\nYou got ' + correct + ' of 7 answers correct!\nTry again?');
+  takeQuiz = confirm (myHomes + '\nYou got ' + correct + ' of 7 answers correct!\nTry again?');
   // if user clicks 'ok' game will start agian
 }
 
